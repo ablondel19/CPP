ebb33bed13f0f1aaa35ffbca7890dfdf4ff1365f
Arnaud Blondel <ablondel@fu-r10-p5.s19.be>
2022-01-10 20:45:34 +0100

    ex05 ok

---
+++ CPP1/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 16:18:42 by ablondel          #+#    #+#             */
+/*   Updated: 2022/01/10 20:16:30 by ablondel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "Karen.hpp"
+
+
+int		main(int ac, char **av)
+{
+	Karen o;
+
+	o.complain("ERROR");
+}
