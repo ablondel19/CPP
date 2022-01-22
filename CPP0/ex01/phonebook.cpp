bd05f2625811a4dcbf7cc2447d571e668658c3ec
Arnaud Blondel <ablondel@fu-r10-p5.s19.be>
2022-01-22 18:27:08 +0100

    CPP0 corrected

--- CPP0/ex01/phonebook.cpp
+++ CPP0/ex01/phonebook.cpp
@@ -6,149 +6,205 @@
 /*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 10:57:22 by ablondel          #+#    #+#             */
-/*   Updated: 2022/01/10 12:05:32 by ablondel         ###   ########.fr       */
+/*   Updated: 2022/01/21 18:47:35 by ablondel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-void	add_contact(int index, phonebook *add)
-	{
-		std::cout << "Enter first name:" << std::endl;
-		std::cin >> add->book[index].first_name;
-		std::cout << "Enter last name:" << std::endl;
-		std::cin >> add->book[index].last_name;
-		std::cout << "Enter nickname:" << std::endl;
-		std::cin >> add->book[index].nickname;
-		std::cout << "Enter phone number:" << std::endl;
-		std::cin >> add->book[index].phone_number;
-		std::cout << "Enter darkest secret:" << std::endl;
-		std::cin >> add->book[index].darkest_secret;
-	}
+std::string phonebook::contact::getFirstName() const
+{
+	return this->_first_name;
+}
 
-	void	print_entry(std::string entry)
+std::string phonebook::contact::getLastName() const
+{
+	return this->_last_name;
+}
+
+std::string phonebook::contact::getNickname() const
+{
+	return this->_nickname;
+}
+
+std::string phonebook::contact::getPhoneNumber() const
+{
+	return this->_phone_number;
+}
+
+std::string phonebook::contact::getSecret() const
+{
+	return this->_darkest_secret;
+}
+
+void 		phonebook::contact::setFirstName( std::string data )
+{
+	this->_first_name = data;
+}
+
+void 		phonebook::contact::setLastName( std::string data )
+{
+	this->_last_name = data;
+}
+
+void 		phonebook::contact::setNickName( std::string data )
+{
+	this->_nickname = data;
+}
+
+void 		phonebook::contact::setPhoneNumber( std::string data )
+{
+	this->_phone_number = data;
+}
+
+void		phonebook::contact::setSecret( std::string data )
+{
+	this->_darkest_secret = data;
+}
+
+void	add_contact(int index, phonebook *addr)
+{
+	std::string data;
+	std::cout << "Enter first name:" << std::endl;
+	std::cin >> data;
+	addr->book[index].setFirstName(data);
+	std::cout << "Enter last name:" << std::endl;
+	std::cin >> data;
+	addr->book[index].setLastName(data);
+	std::cout << "Enter nickname:" << std::endl;
+	std::cin >> data;
+	addr->book[index].setNickName(data);
+	std::cout << "Enter phone number:" << std::endl;
+	std::cin >> data;
+	addr->book[index].setPhoneNumber(data);
+	std::cout << "Enter darkest secret:" << std::endl;
+	std::cin >> data;
+	addr->book[index].setSecret(data);
+}
+
+void	print_entry(std::string entry)
+{
+	std::cout << std::setw(10);
+	if (entry.length() > 10)
+	{
+		entry.resize(10);
+		entry.replace(9, 1, ".");
+	}
+	std::cout << entry;
+	std::cout << "|";
+}
+	
+void	print_contact_infos(phonebook::contact book)
+{
+	std::cout << "first_name: " << book.getFirstName() << std::endl;
+	std::cout << "last_name: " << book.getLastName() << std::endl;
+	std::cout << "nickname: " << book.getNickname() << std::endl;
+	std::cout << "phone_number: " << book.getPhoneNumber() << std::endl;
+	std::cout << "darkest_secret: " << book.getSecret() << std::endl;
+}
+
+void	select_contact(phonebook add)
+{
+	int	x = -1;
+	std::cout << "View informations by entering the contact index" << std::endl;
+	std::cin >> x;
+	while (std::cin.fail() || (x < 1 || x > add.max) || !x)
+	{
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+		std::cout << "Invalid contact number" << std::endl;
+		std::cin >> x;
+	}
+	std::cout << "------------------------------------------------" << std::endl;
+	std::cout << "               CONTACT No: [" << x << "]" << std::endl;
+	std::cout << "------------------------------------------------" << std::endl;
+	std::cout << "first_name: " << add.book[x].getFirstName() << std::endl;
+	std::cout << "last_name: " << add.book[x].getLastName() << std::endl;
+	std::cout << "nickname: " << add.book[x].getNickname() << std::endl;
+	std::cout << "phone_number: " << add.book[x].getPhoneNumber() << std::endl;
+	std::cout << "darkest_secret: " << add.book[x].getSecret() << std::endl;
+	std::cout << "------------------------------------------------" << std::endl;
+}
+	
+void	print_search(phonebook add)
+{
+	int index = 1;
+	
+	if (add.max < 1)
+	{
+		std::cout << "No data available." << std::endl;
+		return ;
+	}
+	std::cout << "   index  |first name| lastname | nickname |" << std::endl;
+	while (index < add.max)
 	{
 		std::cout << std::setw(10);
-		if (entry.length() > 10)
-		{
-			entry.resize(10);
-			entry.replace(9, 1, ".");
-		}
-		std::cout << entry;
-		std::cout << "|";
+		std::cout << index << "|";
+		print_entry(add.book[index].getFirstName());
+		print_entry(add.book[index].getLastName());
+		print_entry(add.book[index].getNickname());
+		std::cout << std::endl;
+		index++;
 	}
+}
+
+void	action_add(phonebook *addr)
+{
+	std::cout << "Add new entry" << std::endl;
+	add_contact(addr->max, addr);
+	addr->max++;
+}
+
+void	action_search(phonebook *add)
+{
+	if (add->max <= 1)
+	{
+		std::cout << "No data available." << std::endl;
+		return ;
+	}
+	std::cout << "Search for entry" << std::endl;
+	print_search(*add);
+	select_contact(*add);
+}
+
+void	action_selection(phonebook::contact *add, phonebook *addr)
+{
+	std::string arg;
 	
-	void	print_contact_infos(phonebook::contact book)
+	if (addr->max <= 8)
+		std::cout << "Enter a request: ADD / SEARCH / EXIT" << std::endl;
+	else
+		std::cout << "Enter a request: SEARCH / EXIT" << std::endl;
+	std::cin >> arg;
+	while (std::cin.fail())
 	{
-		std::cout << "first_name: " << book.first_name << std::endl;
-		std::cout << "last_name: " << book.last_name << std::endl;
-		std::cout << "nickname: " << book.nickname << std::endl;
-		std::cout << "phone_number: " << book.phone_number << std::endl;
-		std::cout << "darkest_secret: " << book.darkest_secret << std::endl;
-	}
-
-	void	select_contact(phonebook add)
-	{
-		int	x = -1;
-		std::cout << "View informations by entering the contact index" << std::endl;
-		std::cin >> x;
-		while (std::cin.fail() || (x < 1 || x > add.max) || !x)
-		{
-			std::cin.clear();
-			std::cin.ignore(INT_MAX, '\n');
-			std::cout << "Invalid contact number" << std::endl;
-			std::cin >> x;
-		}
-		std::cout << "------------------------------------------------" << std::endl;
-		std::cout << "               CONTACT No: [" << x << "]" << std::endl;
-		std::cout << "------------------------------------------------" << std::endl;
-		std::cout << "first_name: " << add.book[x].first_name << std::endl;
-		std::cout << "last_name: " << add.book[x].last_name << std::endl;
-		std::cout << "nickname: " << add.book[x].nickname << std::endl;
-		std::cout << "phone_number: " << add.book[x].phone_number << std::endl;
-		std::cout << "darkest_secret: " << add.book[x].darkest_secret << std::endl;
-		std::cout << "------------------------------------------------" << std::endl;
-	}
-	
-	void	print_search(phonebook add)
-	{
-		int index;
-
-		index = 1;
-		if (add.max < 1)
-		{
-			std::cout << "No data available." << std::endl;
-			return ;
-		}
-		std::cout << "   index  |first name| lastname | nickname |" << std::endl;
-		while (index < add.max)
-		{
-			std::cout << std::setw(10);
-			std::cout << index << "|";
-			print_entry(add.book[index].first_name);
-			print_entry(add.book[index].last_name);
-			print_entry(add.book[index].nickname);
-			std::cout << std::endl;
-			index++;
-		}
-	}
-
-	void	action_add(phonebook *add)
-	{
-		std::cout << "Add new entry" << std::endl;
-		add_contact(add->max, add);
-		add->max++;
-	}
-
-	void	action_search(phonebook *add)
-	{
-		if (add->max <= 1)
-		{
-			std::cout << "No data available." << std::endl;
-			return ;
-		}
-		std::cout << "Search for entry" << std::endl;
-		print_search(*add);
-		select_contact(*add);
-	}
-
-	void	action_selection(phonebook *add)
-	{
-		std::string arg;
-		
-		if (add->max <= 8)
-			std::cout << "Enter a request: ADD / SEARCH / EXIT" << std::endl;
-		else
-			std::cout << "Enter a request: SEARCH / EXIT" << std::endl;
+		std::cin.clear();
+		std::cin.ignore(MAX_INPUT, '\n');
+		std::cout << "Invalid input" << std::endl;
 		std::cin >> arg;
-		while (std::cin.fail())
-		{
-			std::cin.clear();
-			std::cin.ignore(MAX_INPUT, '\n');
-			std::cout << "Invalid input" << std::endl;
-			std::cin >> arg;
-		}
-		if (arg.compare("ADD") == 0)
-			action_add(add);
-		else if (arg.compare("SEARCH") == 0)
-			action_search(add);
-		else if (arg.compare("EXIT") == 0)
-			exit(0);
-		else
-			std::cout << "Invalid request" << std::endl;
-		if (add->max > 8)
-		{
-			std::cout << "Phonebook full" << std::endl;
-			action_selection(add);
-		}
 	}
+	if (arg.compare("ADD") == 0)
+		action_add(addr);
+	else if (arg.compare("SEARCH") == 0)
+		action_search(addr);
+	else if (arg.compare("EXIT") == 0)
+		exit(0);
+	else
+		std::cout << "Invalid request" << std::endl;
+	if (addr->max > 8)
+	{
+		std::cout << "Phonebook full" << std::endl;
+		action_selection(add, addr);
+	}
+}
 
 int		main(void)
 {	
-	phonebook add;
+	phonebook addr;
+	phonebook::contact add; 
 	
-	add.max = 1;
+	addr.max = 1;
 	while (1)
-		action_selection(&add);
+		action_selection(&add, &addr);
 	return (0);
 }
