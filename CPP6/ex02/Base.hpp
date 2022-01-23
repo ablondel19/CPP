52999831e2db8ddee84938af8f4d81435c555802
Arnaud Blondel <ablondel@shi-r3-p4.s19.be>
2022-01-23 22:29:36 +0100

    CPP6 OK

--- CPP6/ex02/Base.hpp
+++ CPP6/ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 20:05:05 by ablondel          #+#    #+#             */
-/*   Updated: 2022/01/23 20:16:14 by ablondel         ###   ########.fr       */
+/*   Updated: 2022/01/23 22:23:02 by ablondel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 #define BASE_HPP
 #include <random>
 #include <iostream>
+#include <exception>
+
+class	Base
+{
+	private:
+		/* data */
+	protected:
+		/* data */
+	public:
+		Base();
+		Base( const Base &obj );
+		Base &operator=( const Base &obj );
+		virtual ~Base();
+};
 
 #endif
--- CPP6/ex02/main.cpp
+++ CPP6/ex02/main.cpp
@@ -6,32 +6,95 @@
 /*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 20:11:33 by ablondel          #+#    #+#             */
-/*   Updated: 2022/01/23 20:18:05 by ablondel         ###   ########.fr       */
+/*   Updated: 2022/01/23 22:24:29 by ablondel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "Base.hpp"
-#include <iostream>
-#include <random>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
-int rdm(int min, int max) //range : [min, max]
+int rdm(int min, int max)
 {
-   static bool first = true;
-   if (first) 
-   {  
-      std::srand( time(NULL) ); //seeding for the first time only!
-      first = false;
-   }
-   return min + std::rand() % (( max + 1 ) - min);
+	static bool first = true;
+	if (first)
+	{
+		std::srand( time(NULL) );
+		first = false;
+	}
+	return min + std::rand() % ((max + 1) - min);
+}
+
+void identify(Base * p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Unknown Type" << std::endl;
+}
+
+void identify( Base & p)
+{
+	try
+	{
+		p = dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch(const std::bad_cast &bc )
+	{
+		//std::cerr << bc.what() << '\n';
+	}
+	try
+	{
+		p = dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+	}
+	catch(const std::bad_cast &bc )
+	{
+		//std::cerr << bc.what() << '\n';
+	}
+	try
+	{
+		p = dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+	}
+	catch(const std::bad_cast &bc )
+	{
+		//std::cerr << bc.what() << '\n';
+	}
+}
+
+Base	*generate( void )
+{
+	Base *X = NULL;
+	
+	switch (rdm(1, 3))
+	{
+		case 1:
+			X = new A();
+			break ;
+		case 2:
+			X = new B();
+			break ;
+		case 3:
+			X = new C();
+			break ;
+		default:
+			std::cout << "Generate failed" << std::endl;
+	}
+	return (X);
 }
 
 int		main( void )
 {
-	int x = 0;
-
-	for (size_t i = 0; i < 10; i++)
-	{
-		x = rdm(1, 3);
-		std::cout << x << std::endl;
-	}
+	Base *X = generate();
+	identify(X);
+	identify(*X);
+	delete X;
+	return 0;
 }
