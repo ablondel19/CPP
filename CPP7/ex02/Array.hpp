337f4fd3065dcd3ce766d4d209fc45ff6fd328a0
Arnaud Blondel <ablondel@shi-r3-p4.s19.be>
2022-01-24 23:58:21 +0100

    CPP7 almost done

--- CPP7/ex02/Array.hpp
+++ CPP7/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 16:08:32 by ablondel          #+#    #+#             */
-/*   Updated: 2022/01/24 16:13:00 by ablondel         ###   ########.fr       */
+/*   Updated: 2022/01/24 23:54:51 by ablondel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,45 @@ template < typename T >
 class	Array
 {
 	private:
-		/* data */
+		T			*_t;
+		unsigned	_size;
 	protected:
 		/* data */
 	public:
-		Array();
-		Array( unsigned int n );
-		Array( const Array &obj );
-		Array &operator=( const Array &obj );
-		~Array();
+		Array()
+		{
+			_t = new T[1];
+			_t[0] = 0;
+		}
+		Array( unsigned int n )
+		{
+			_t = new T[n];
+			this->_size = n;
+			for (unsigned i = 0; i < n; i++)
+			{
+				_t[i] = 0;
+				std::cout << "[" << _t[i] << "]";
+			}
+			std::cout << std::endl;
+		}
+		Array( const Array &obj )
+		{
+
+		}
+		Array &operator=( const T &obj )
+		{
+			
+		}
+		T &operator[](T index)
+		{
+			if (index >= size)
+				std::cout << "Exception" << std::endl;
+			return _t[index];
+		}
+		~Array()
+		{
+			delete[] this->_t;
+		}
 };
 
 
--- CPP7/ex02/main.cpp
+++ CPP7/ex02/main.cpp
@@ -6,8 +6,61 @@
 /*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 16:11:11 by ablondel          #+#    #+#             */
-/*   Updated: 2022/01/24 16:11:19 by ablondel         ###   ########.fr       */
+/*   Updated: 2022/01/24 23:41:22 by ablondel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Array.hpp"
+
+#define MAX_VAL 750
+int main(int, char**)
+{
+	Array<int>(10);
+    /*Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//*/
+    return 0;
+}
