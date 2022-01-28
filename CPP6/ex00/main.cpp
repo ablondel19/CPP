e502feacd6beab86de162060f87a9a4f120c1443
Arnaud Blondel <ablondel@fu-r10-p5.s19.be>
2022-01-28 20:09:46 +0100

    CPP6 seems OK

--- CPP6/ex00/main.cpp
+++ CPP6/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 19:06:10 by ablondel          #+#    #+#             */
-/*   Updated: 2022/01/23 16:34:01 by ablondel         ###   ########.fr       */
+/*   Updated: 2022/01/28 20:09:17 by ablondel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ int		main( int ac, char **av )
 {
 	if (ac == 2)
 	{
-		static_cast<void>(static_cast<char>(Conv((std::string)av[1])));
-		static_cast<void>(static_cast<int>(Conv((std::string)av[1])));
-		static_cast<void>(static_cast<float>(Conv((std::string)av[1])));
-		static_cast<void>(static_cast<double>(Conv((std::string)av[1])));
+		char c = 0;
+		int i = 0;
+		float f = 0.0f;
+		double d = 0.0;
+		c = static_cast<char>(Conv((std::string)av[1]));
+		i = static_cast<int>(Conv((std::string)av[1]));
+		f = static_cast<float>(Conv((std::string)av[1]));
+		d = static_cast<double>(Conv((std::string)av[1]));
 	}
 }
